754b470d96ce9321b5629d38ba5eff4337a2bccd
Jon Alter <jon@local>
2012-11-16 11:46:31 -0800

    [MOD-1017] Add documentation, clean up example, remove unnecessary files

--- commonjs/example/app.js
+++ commonjs/example/app.js
@@ -1,59 +1,49 @@
 var NAVIBRIDGE = require('ti.navibridge');
-NAVIBRIDGE.SetApplicationID('ICiAV4Ay');
 
-var win = Ti.UI.createWindow({ title : "navibridge" });
+// ApplicationId must be set before adding POIs NaviBridge
+// See the documentation for how to get an AppicationId
+// NAVIBRIDGE.setApplicationId('<< YOUR APPLICATION ID HERE >>');
 
-var topView = Ti.UI.createView({});
- 
+var win = Ti.UI.createWindow({ title:"navibridge" });
 
-var naviView = Ti.UI.createView({ top: 0, height: 600 });
-		
-		
-		var versionLabel = Ti.UI.createLabel({
-				top: 0, left: 0, height: 10,
-				color: 'black',
-				text:"v" + Titanium.App.getVersion() + " ",
-				font: {fontSize: 6}
-		});//end versionLabel	
-		naviView.add( versionLabel );
-		
-		//and the trigger button
-		var OpenButton = Ti.UI.createButton({ top:5, title:'Open NaviBridge', height:40, width:200 });
-		OpenButton.addEventListener('click', function(){ NAVIBRIDGE.openNavi(); });
-		naviView.add( OpenButton );
-		
-		var InstallButton = Ti.UI.createButton({ top:50, title:'Install NaviBridge', height:40, width:200 });
-		InstallButton.addEventListener('click', function(){ NAVIBRIDGE.installNavi();  });
-		naviView.add( InstallButton );
-		
-		var InsertPOIButton = Ti.UI.createButton({ top:95, title:'Insert POI', height:40, width:200 });
-		InsertPOIButton.addEventListener('click', function(){ NAVIBRIDGE.addPOI({ lat:37.38922, lon:-122.048496});  });
-		naviView.add( InsertPOIButton );
-		
-		//add the console
-		var console = Ti.UI.createLabel({
-				top: 200, left: 5, right: 5, height: 160,
-				backgroundColor: 'white', color: 'black',
-				text:" ... waiting ...",
-				font: {fontSize: 12}, verticalAlign: 'top'
-		});
-		naviView.add( console )
-		
-		function addMsg(msg) {
-			var text = console.text;
-			if (text && text.length > 0) {
-				text = msg + '\n' + text;
-			} else {
-				text = msg;
-			}
-			console.text = text;
-		}//end addMsg
-		
-		Ti.App.addEventListener('ti.navibridge.admin.console', function(data) {
-			addMsg('admin.console: "' + data.message + '"');
-		});
-	
-	topView.add( naviView );
-	
-	win.add( topView );
+var openButton = Ti.UI.createButton({ top:5, title:'Open NaviBridge', height:40, width:200 });
+var installButton = Ti.UI.createButton({ top:50, title:'Install NaviBridge', height:40, width:200 });
+var insertPOIButton = Ti.UI.createButton({ top:95, title:'Insert POI', height:40, width:200 });
+var insertMultiPOIsButton = Ti.UI.createButton({ top:140, title:'Insert Multiple POIs', height:40, width:200 });
+
+openButton.addEventListener('click', function(){ 
+	NAVIBRIDGE.openNavi(); 
+});
+installButton.addEventListener('click', function(){ 
+	NAVIBRIDGE.installNavi();  
+});
+insertPOIButton.addEventListener('click', function(){ 
+	NAVIBRIDGE.addPOI({
+		title:'My POI',
+		lat:37.38922, 
+		lon:-122.048496
+	});  
+});
+insertMultiPOIsButton.addEventListener('click', function(){ 
+	NAVIBRIDGE.addMultiPOI({
+		poi: [
+			{
+				title:'My POI',
+				lat:37.38922, 
+				lon:-122.048496
+			},
+			{
+				title:'My Other POI',
+				lat:40.6214, 
+				lon:-79.1528
+			},
+		]
+	});
+});
+
+win.add( openButton );
+win.add( installButton );
+win.add( insertPOIButton );
+win.add( insertMultiPOIsButton );
+
 win.open();
